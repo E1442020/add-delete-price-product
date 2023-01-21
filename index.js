de5fc2bb3c4ee7f575234d8af8e5dcf0fe927079
E1442020 <114053062+E1442020@users.noreply.github.com>
2023-01-21 12:33:21 +0200

    list

---
+++ index.js
@@ -0,0 +1,116 @@
+"use strict";
+let insertProductName = document.querySelector(".insert-product-name");
+let insertProductPrice = document.querySelector(".insert-product-price");
+let insertProductQuantity = document.querySelector(".insert-product-quantity");
+let form = document.querySelector("form");
+let productItemContainer = document.querySelector(".product-item-container");
+let total = document.querySelector(".total");
+const getProductInfoFromLocal = () => {
+    let productInfo = localStorage.getItem("productInfo");
+    if (productInfo === null) {
+        return [];
+    }
+    return JSON.parse(productInfo);
+};
+let productArr = getProductInfoFromLocal();
+let checkProductInformation = () => {
+    let displayProduct = document.querySelector(".display-product");
+    console.log(productArr);
+    if (productArr.length == 0) {
+        displayProduct.style.display = "block";
+        console.log("if");
+    }
+    else {
+        displayProduct.style.display = "none";
+        console.log("else");
+    }
+};
+checkProductInformation();
+let setProductToLocalStorage = () => {
+    localStorage.setItem("productInfo", JSON.stringify(productArr));
+    getTotal();
+    checkProductInformation();
+};
+let createProduct = () => {
+    if ((insertProductName === null || insertProductName === void 0 ? void 0 : insertProductName.value.trim()) == "" ||
+        (insertProductPrice === null || insertProductPrice === void 0 ? void 0 : insertProductPrice.value) == null ||
+        (insertProductQuantity === null || insertProductQuantity === void 0 ? void 0 : insertProductQuantity.value) == null)
+        return;
+    let id;
+    if (productArr.length <= 0) {
+        id = 1;
+    }
+    else {
+        id = productArr[productArr.length - 1].id + 1;
+    }
+    const newProduct = {
+        id: id,
+        productName: insertProductName.value,
+        productPrice: insertProductPrice.value,
+        productQuantity: insertProductQuantity.value,
+    };
+    productArr.push(newProduct);
+    setProductToLocalStorage();
+    addNewProduct(newProduct);
+};
+let addNewProduct = (product) => {
+    let content = "";
+    content += `
+            <div class="items">
+                <p>${product.productName}</p>
+               <p>${product.productPrice}</p>
+               <p>${product.productQuantity}</p>
+               <button data-id=${product.id} class=" remove-product"><i class="fa-solid fa-xmark "></i></button>
+                </div>
+                `;
+    productItemContainer.innerHTML += content;
+    removeProduct();
+};
+let removeProduct = () => {
+    let removeProducts = document.querySelectorAll(".remove-product");
+    removeProducts.forEach((removeButton) => {
+        removeButton.addEventListener("click", () => {
+            // console.log(contactArr)
+            productArr.map((productItem, index) => {
+                let stringProductId = removeButton === null || removeButton === void 0 ? void 0 : removeButton.dataset.id;
+                if (stringProductId !== undefined) {
+                    let productIdNumber = parseInt(stringProductId);
+                    if (productItem.id == productIdNumber) {
+                        productArr.splice(index, 1);
+                        let productInfo = removeButton.closest(".items");
+                        if (productInfo !== null) {
+                            productInfo.style.display = "none";
+                        }
+                    }
+                }
+            });
+            setProductToLocalStorage();
+        });
+    });
+};
+let getTotal = () => {
+    let totalPrice = 0;
+    if (productArr.length == 0) {
+        totalPrice = 0;
+        let stringTotalPrice = totalPrice.toString();
+        total.innerHTML = stringTotalPrice;
+    }
+    else {
+        productArr.map((item) => {
+            let productPriceNumber = parseInt(item.productPrice);
+            let productQuantityNumber = parseInt(item.productQuantity);
+            totalPrice += productPriceNumber * productQuantityNumber;
+            let stringTotalPrice = totalPrice.toString();
+            total.innerHTML = stringTotalPrice;
+        });
+    }
+};
+getTotal();
+productArr.forEach(addNewProduct);
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createProduct();
+    insertProductName.value = "";
+    insertProductPrice.value = "";
+    insertProductQuantity.value = "";
+});
---
+++ index.ts
@@ -0,0 +1,152 @@
+let insertProductName = document.querySelector(
+  ".insert-product-name"
+) as HTMLInputElement;
+let insertProductPrice = document.querySelector(
+  ".insert-product-price"
+) as HTMLInputElement;
+
+let insertProductQuantity = document.querySelector(
+  ".insert-product-quantity"
+) as HTMLInputElement;
+let form = document.querySelector("form") as HTMLFormElement;
+let productItemContainer = document.querySelector(
+  ".product-item-container"
+) as HTMLDivElement;
+let total = document.querySelector(".total") as HTMLSpanElement;
+
+interface Product {
+  id: number;
+  productName: string;
+  productPrice: string;
+  productQuantity: string;
+}
+
+const getProductInfoFromLocal = (): Product[] => {
+  let productInfo = localStorage.getItem("productInfo");
+  if (productInfo === null) {
+    return [];
+  }
+  return JSON.parse(productInfo);
+};
+
+let productArr: Product[] = getProductInfoFromLocal();
+
+let checkProductInformation = (): void => {
+  let displayProduct = document.querySelector(
+    ".display-product"
+  ) as HTMLParagraphElement;
+
+  console.log(productArr);
+  if (productArr.length == 0) {
+    displayProduct.style.display = "block";
+    console.log("if");
+  } else {
+    displayProduct.style.display = "none";
+    console.log("else");
+  }
+};
+
+checkProductInformation();
+
+let setProductToLocalStorage = (): void => {
+  localStorage.setItem("productInfo", JSON.stringify(productArr));
+  getTotal();
+  checkProductInformation();
+};
+
+let createProduct = (): void => {
+  if (
+    insertProductName?.value.trim() == "" ||
+    insertProductPrice?.value == null ||
+    insertProductQuantity?.value == null
+  )
+    return;
+
+  let id: number;
+  if (productArr.length <= 0) {
+    id = 1;
+  } else {
+    id = productArr[productArr.length - 1].id + 1;
+  }
+
+  const newProduct: Product = {
+    id: id,
+    productName: insertProductName.value,
+    productPrice: insertProductPrice.value,
+    productQuantity: insertProductQuantity.value,
+  };
+  productArr.push(newProduct);
+
+  setProductToLocalStorage();
+
+  addNewProduct(newProduct);
+};
+
+let addNewProduct = (product: Product): void => {
+  let content: string = "";
+
+  content += `
+            <div class="items">
+                <p>${product.productName}</p>
+               <p>${product.productPrice}</p>
+               <p>${product.productQuantity}</p>
+               <button data-id=${product.id} class=" remove-product"><i class="fa-solid fa-xmark "></i></button>
+                </div>
+                `;
+  productItemContainer.innerHTML += content;
+  removeProduct();
+};
+
+let removeProduct = (): void => {
+  let removeProducts =
+    document.querySelectorAll<HTMLButtonElement>(".remove-product");
+
+  removeProducts.forEach((removeButton) => {
+    removeButton.addEventListener("click", () => {
+      // console.log(contactArr)
+      productArr.map((productItem, index) => {
+        let stringProductId: string | undefined = removeButton?.dataset.id;
+        if (stringProductId !== undefined) {
+          let productIdNumber: number = parseInt(stringProductId);
+          if (productItem.id == productIdNumber) {
+            productArr.splice(index, 1);
+            let productInfo = removeButton.closest(".items") as HTMLDivElement;
+            if (productInfo !== null) {
+              productInfo.style.display = "none";
+            }
+          }
+        }
+      });
+
+      setProductToLocalStorage();
+    });
+  });
+};
+
+let getTotal = () => {
+  let totalPrice = 0;
+  if (productArr.length == 0) {
+    totalPrice = 0;
+    let stringTotalPrice = totalPrice.toString();
+    total.innerHTML = stringTotalPrice;
+  } else {
+    productArr.map((item) => {
+      let productPriceNumber: number = parseInt(item.productPrice);
+      let productQuantityNumber: number = parseInt(item.productQuantity);
+      totalPrice += productPriceNumber * productQuantityNumber;
+      let stringTotalPrice = totalPrice.toString();
+      total.innerHTML = stringTotalPrice;
+    });
+  }
+};
+getTotal();
+productArr.forEach(addNewProduct);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createProduct();
+
+  insertProductName.value = "";
+  insertProductPrice.value = "";
+  insertProductQuantity.value = "";
+});
